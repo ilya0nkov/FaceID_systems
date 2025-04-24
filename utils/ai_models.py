c0cee10a53df760bbc52c3832aaf383f927f2a15
ilya <ilya.on2003@gmail.com>
2025-04-24 22:12:30 +0500

    add cuda

--- utils/ai_models.py
+++ utils/ai_models.py
@@ -5,5 +5,6 @@ THRESHOLD = 0.6  # Порог схожести
 
 # Инициализация модели
 face_app = FaceAnalysis(name="buffalo_l", providers=["CPUExecutionProvider"])
+# face_app = FaceAnalysis(name="buffalo_l", providers=["CUDAExecutionProvider"])
 face_app.prepare(ctx_id=0)
 print("модель загружена")
